98dcbbd96e6db7d0c7d1f1a21d23ffca173eb244
Phyya <80838239+Phyya@users.noreply.github.com>
2022-03-14 23:59:49 +0100

    Updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - Prints numbers between 0 to 9 with commas and spaces.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
