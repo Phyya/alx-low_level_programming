33dd21ace9fc0bdd0039621420f7cc21291ce15b
Phyya <phaboolous11@gmail.com>
2022-03-14 16:15:08 -0700

    commands

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints combinations of three digits
- *
- * Return: Always 0 (Success)
- */
+* main - Prints combinations of three digits
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int i, j;
