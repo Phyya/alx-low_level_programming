6e1364ad6b2ba506db411fa5ab5fc7ea48bb0844
Phyya <phaboolous11@gmail.com>
2022-03-16 13:10:13 -0700

    check for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* _isalpha - checks for alphabetic character* @c: c is an ascii character
+* _isalpha - checks for alphabetic character
+* @c: c is an ascii character
 *
 * Return: 1 (if letter)
 */
